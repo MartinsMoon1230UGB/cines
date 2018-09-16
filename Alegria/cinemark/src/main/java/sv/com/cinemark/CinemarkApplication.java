3181cb36f1f286eec604459ca5af8684ebea1d25
MartinsMoon1230UGB <martinsmoon1230@gmail.com>
2018-09-16 19:58:56 +0000

    Add files via upload

---
+++ Alegria/cinemark/src/main/java/sv/com/cinemark/CinemarkApplication.java
@@ -0,0 +1,12 @@
+package sv.com.cinemark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemarkApplication.class, args);
+	}
+}
